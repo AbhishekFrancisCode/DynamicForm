47ec1c261439b635cb056c154bd9f91370c7ba21
abhishek <abhishek@finemake.in>
2023-06-26 13:35:19 +0530

    new changes

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Dynamic from "./dynamicForm";
+
+export default function Home() {
+  return (
+    <div className="wrapper w-full h-screen bg-white pt-40 pl-20">
+      <Dynamic />
+      </div>
+  )
+}
